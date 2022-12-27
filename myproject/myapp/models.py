6196b6b9a5ed0e2ea64b04f111540b71e91dd050
subhashinimayrya <subhashini20@navgurukul.org>
2022-12-27 04:13:55 -0800

    first commit

---
+++ myproject/myapp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Article(models.Model):
+    title=models.CharField(max_length=250)
+    name=models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.title
+
+
+
